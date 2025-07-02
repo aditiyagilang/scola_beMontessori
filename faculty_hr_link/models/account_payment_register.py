04bbd75f100896be658def90bbfd5c71a873614d
Aditiya Gilang <aditiyagilang031@gmail.com>
2025-07-02 10:32:40 +0700

    update payroll send

--- faculty_hr_link/models/account_payment_register.py
+++ faculty_hr_link/models/account_payment_register.py
@@ -11,6 +11,21 @@ class AccountPaymentRegister(models.TransientModel):
 
         if active_model == 'account.move' and len(active_ids) == 1:
             move = self.env['account.move'].browse(active_ids[0])
-            if move.exists() and move.ref:
-                res['communication'] = move.ref
+
+            if move.exists():
+                ref_text = move.ref or move.name or ""
+                res['communication'] = ref_text
+                keywords = ['SLIP', 'Salary', 'Gaji', 'Net Salary']
+                if any(word.lower() in ref_text.lower() for word in keywords):
+                    res['payment_type'] = 'outbound' 
+
         return res
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    @api.model
+    def create(self, vals):
+        if vals.get('ref') and any(k in vals['ref'].lower() for k in ['slip', 'salary', 'gaji']):
+            vals['payment_type'] = 'outbound'
+        return super().create(vals)
